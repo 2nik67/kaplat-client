fa8c9766054433a74f5e4da082f592a6d4c9ee58
2nik67 <2niknatan@gmail.com>
2022-11-27 15:22:09 +0200

    Adding PUT and DELETE

--- client.py
+++ client.py
@@ -2,14 +2,30 @@ import requests
 import datetime
 
 if __name__ == '__main__':
-    get_response = requests.get('http://localhost:8989/test_get_method', params={'hour': datetime.datetime.now().hour,
-                                                                      'minute': datetime.datetime.now().minute})
-    print(get_response.text)
+    current_hour = datetime.datetime.now().hour
+    current_minute = datetime.datetime.now().minute
+    # GET
+    get_response = requests.get('http://localhost:8989/test_get_method', params={'hour': current_hour,
+                                                                                 'minute': current_minute})
+    # POST
     post_body = {
-        "hour": datetime.datetime.now().hour,
-        "minute": datetime.datetime.now().minute,
+        "hour": current_hour,
+        "minute": current_minute,
         "requestId": get_response.text
     }
-    x = requests.post('http://localhost:8989/test_post_method', data=post_body)
-    message = x.json()['message']
-    print(message)
+    post_response = requests.post('http://localhost:8989/test_post_method', data=post_body)
+    post_message = post_response.json()['message']
+
+    current_hour = (current_hour + 21) % 24
+    current_minute = (current_minute + 13) % 60
+
+    # PUT
+    put_body = {
+        "hour": current_hour,
+        "minute": current_minute
+    }
+    put_response = requests.put("http://localhost:8989/test_put_method", params={"id": post_message}, data=put_body)
+    put_message = put_response.json()['message']
+
+    # DELETE
+    requests.delete("http://localhost:8989/test_put_method", params={"id": put_message})
--- server.py
+++ server.py
@@ -8,6 +8,7 @@ port = 8989
 app = Flask(__name__)
 get_response = '45t5y6-y6y-6y6-u7i75t5-5t5t'
 post_response = '1q1-3ww-2-s2–ee-ss-d'
+put_response = 'hhfg7rt5-t5–grn-h-j7-6ygr'
 
 
 @app.route('/test_get_method', methods=['GET'])
@@ -17,43 +18,18 @@ def do_get():
 
 @app.route('/test_post_method', methods=['POST'])
 def do_post():
-    print(request.form['hour'])
     return jsonify({'message': post_response})
 
 
-@app.route('/test_put_method', methods=['DELETE'])
+@app.route('/test_put_method', methods=['PUT'])
 def do_put():
-    return "put"
+    return jsonify({"message": put_response})
 
 
 @app.route('/test_delete_method', methods=['DELETE'])
 def do_delete():
     return "delete"
-# class Server(BaseHTTPRequestHandler):
-#     def do_GET(self):
-#         if 'test_get_method' in self.path:
-#             params = urlparse(self.path).query
-#             query_components = dict(qc.split("=") for qc in params.split("&"))
-#             print(query_components['hour'])
-#         self.send_response(200)
-#         self.send_header('Content-type', 'text')
-#         self.end_headers()
-#         # Send the html message
-#         self.wfile.write(bytes("45t5y6-y6y-6y6-u7i75t5-5t5t", "utf-8"))
-#         return
-#
-#     def do_POST(self):
-#         self.send_response(200)
-#         self.send_header('Content-type', 'application/json')
-#         self.end_headers()
-#         # Send the html message
-#         self.wfile.write(bytes('{"name": "natan"}', "utf-8"))
-#         return
 
 
 if __name__ == '__main__':
     app.run(host=host, port=port)
-    # server = HTTPServer((host, port), Server)
-    # print("server starting....")
-    # server.serve_forever()
-    # server.server_close()
